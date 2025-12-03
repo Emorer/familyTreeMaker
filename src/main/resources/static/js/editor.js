89f6fdfe913b4eac6f20b7235c6322768faa1d8e
ziad <ziad_01@hotmail.de>
2025-12-03 18:07:05 +0100

    New start canvas für das Koordinatensystem und zoomen wieder eingefügt

--- src/main/resources/static/js/editor.js
+++ src/main/resources/static/js/editor.js
@@ -1,53 +1,46 @@
 
-let zoom = 1;
-let posX = 0;
-let posY = 0;
-let isPanning = false;
-let startX, startY;
+const root = document.documentElement;
 
-const env = document.querySelector('.environment');
-const wrapper = document.querySelector('.environment-wrapper');
+// draw the Koordinatensystem
+var CanvasCoord = document.getElementById("CanvasCoord");
+var width = CanvasCoord.clientWidth;
+var height = CanvasCoord.clientHeight;
+var CoordDensity = 25;
 
-env.addEventListener('wheel', (event) => {
+var ctx = CanvasCoord.getContext("2d");
+// do the vertical lines
+for(let i = CoordDensity; i < width; i += CoordDensity) {
+    ctx.moveTo(i, 0);
+    ctx.lineTo(i, height);
+}
+
+// do the horizontal lines
+for(let i = CoordDensity; i < height; i += CoordDensity){
+    ctx.moveTo(0, i);
+    ctx.lineTo(width, i);
+}
+
+ctx.stroke();
+///////////////////////////////////////////////////////////////////
+// zooming
+const content = document.querySelector(".container div.content");
+let zoom = 100;
+const zoomString = getComputedStyle(root).getPropertyValue("--zoom"); // current zoom
+content.addEventListener("wheel", (event) =>{
     event.preventDefault();
-
-    if (event.deltaY < 0) {
-        zoom += 0.1;    //zoom in
+        if (event.deltaY < 0) {
+        zoom += 1;    //zoom in
     } else {
-        zoom -= 0.1; // zoom out
+        zoom -= 1; // zoom out
     }
 
-    zoom = Math.max(0.5 ,Math.min(zoom, 3));
-
-    env.style.transform = `scale(${zoom})`;
+    zoom = Math.max(50 ,Math.min(zoom, 300));
+    zooming(zoom);
 });
 
 
 
+function zooming(newZoom){
+    root.style.setProperty("--zoom", newZoom + "%");
 
-
-wrapper.addEventListener('mousedown', (e) => {
-    isPanning = true;
-    startX = e.clientX - posX;
-    startY = e.clientY - posY;
-});
-
-wrapper.addEventListener('mousemove', (e) => {
-    if (!isPanning) return;
-    posX = e.clientX - startX;
-    posY = e.clientY - startY;
-    updateTransform();
-});
-
-wrapper.addEventListener('mouseup', () => {
-    isPanning = false;
-});
-
-wrapper.addEventListener('mouseleave', () => {
-    isPanning = false;
-});
-
-
-function updateTransform() {
-    env.style.transform = `translate(${posX}px, ${posY}px) scale(${zoom})`;
 }
